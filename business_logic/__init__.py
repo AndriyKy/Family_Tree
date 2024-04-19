91099f9692012f32144ba82293ace82171101e37
andriyky <akrechovetskiy@gmail.com>
2024-04-19 20:00:32 +0300

    refactor: import graph

--- business_logic/__init__.py
+++ business_logic/__init__.py
@@ -1,5 +1,6 @@
 from .add_edit import AddEdit
 from .add_remove_clan import AddRemoveClan
 from .family_ties import FamilyTies
+from .graph import Graph
 from .my_card import MyCard
 from .review import Review
